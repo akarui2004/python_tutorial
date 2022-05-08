65083c633b140186228570cf4d42e7160906149a
Akarui <minhdpc@gmail.com>
2022-05-08 22:36:49 +0700

    1. Basic Syntax Python in the link 2. Python Comments

---
+++ 1_Python_Basics/1_Basic_Python_Syntax.md
@@ -0,0 +1,3 @@
+# Python Basic Syntax
+- [W3School Basic Syntax](https://www.w3schools.com/python/python_syntax.asp)
+- [Tutorial Basic Syntax](https://www.tutorialspoint.com/python/python_basic_syntax.htm)
---
+++ 1_Python_Basics/2_Python_Comments.py
@@ -0,0 +1,11 @@
+# Python Comments
+#This is a comment
+
+# Multi-line comments
+#This is a long comment
+#and it extends
+#to multiple lines
+
+"""This is also a
+perfect example of
+multi-line comments"""
